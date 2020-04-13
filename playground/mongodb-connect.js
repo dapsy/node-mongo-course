290fd3549cce6709a0a82c0a4c868154db34a181
Adedapo Fabowale <babyface40002001@yahoo.com>
2020-04-13 22:14:10 +0100

    all node REST api work

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,62 +1,35 @@
 // const MongoClient = require('mongodb').MongoClient;
 const {MongoClient, ObjectID} = require('mongodb');
 
-// var obj = new ObjectID;
-// console.log(obj);
-// var user = {name: "Adedapo", age: 25};
-// var {name} = user;
-// console.log(name);
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
 
-MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
-if (err) {
-   return console.log('Unable to connect to mongodb server')
-}
-console.log('Connected to MongoDB server');
-//const db = client.db('TodoApp')
+  // db.collection('Todos').insertOne({
+  //   text: 'Something to do',
+  //   completed: false
+  // }, (err, result) => {
+  //   if (err) {
+  //     return console.log('Unable to insert todo', err);
+  //   }
+  //
+  //   console.log(JSON.stringify(result.ops, undefined, 2));
+  // });
 
-// db.collection('Todos').insertOne({
-//     text: "Something to do",
-//     completed: false
-// }, (err, result) => {
-//     if (err) {
-//         return console.log('Unable to insert Todo', err);
-//     }
-//     console.log(JSON.stringify(result.ops, undefined, 2));
-// });
+  // Insert new doc into Users (name, age, location)
+  // db.collection('Users').insertOne({
+  //   name: 'Andrew',
+  //   age: 25,
+  //   location: 'Philadelphia'
+  // }, (err, result) => {
+  //   if (err) {
+  //     return console.log('Unable to insert user', err);
+  //   }
+  //
+  //   console.log(result.ops[0]._id.getTimestamp());
+  // });
 
-//
-// db.collection('Users').insertOne({
-//     name: 'Adedapo Fabowale',
-//     age: 29,
-//     location: 'Lagos',
-//     completed: false
-// }, (err, result) => {
-//     if (err) {
-//         return console.log('Unable to insert Users', err);
-//     }
-//     console.log(result.ops[0]._id);
-// });
-// client.close();
-// });
-
-// const MongoClient = require('mongodb').MongoClient;
-// const assert = require('assert');
-
-// (async function() {
-//   // Connection URL
-//   const url = 'mongodb://localhost:27017/TodoApp';
-//   // Database Name
-//   const dbName = 'TodoApp';
-//   const client = new MongoClient(url, { useNewUrlParser: true });
-
-//   try {
-//     // Use connect method to connect to the Server
-//     await client.connect();
-
-//     const db = client.db(dbName);
-//   } catch (err) {
-//     console.log(err.stack);
-//   }
-
-  client.close();
- });
+  db.close();
+});
